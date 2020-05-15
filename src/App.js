f7e57677c0956ceaa3dd6075d70b08ece537262e
Tran Nguyen Dang Khoi <khoi.tran@infodation.local>
2020-05-15 13:58:35 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App(){
-  const [count, setCount] = useState(0);
-  const [text_shown,settext_shown] =useState('');
+function App() {
+    const [count, setCount] = useState(0);
+    const [text_shown, settext_shown] = useState('');
 
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
-  return (
-    <div>
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
+    // const a= 
+    return (
+        <div>
       <p>You clicked {count} times</p>
       <button onClick={() => {
           setCount(count + 1);
-          settext_shown(text_shown + 'Text is shown, ');
+          settext_shown(text_shown +"Text is shown, ");
         }
       }>
         Click me
       </button>
       <br />
-      <p>{text_shown}</p>
+      {text_shown && text_shown.split ('\n').map ((item, i) => {return <p key={i}>{item}</p>;})}
     </div>
-  );
+    );
 }
-  
+
 export default App;
